5749bad9510c9a8d8350c9dee8cc9918cac88996
Ooi Yu Zhang <yooi0010@student.monash.edu>
2023-05-02 17:01:29 +0800

    Added completed Task 1 of SQL Portfolio 1.

--- Applied/App08/SQLPortfolio1/sql_portfolio1.sql
+++ Applied/App08/SQLPortfolio1/sql_portfolio1.sql
@@ -1,25 +1,58 @@
 --Comment out SET ECHO and SPOOL commands before submitting your portfolio
-SET ECHO ON
-SPOOL sql_portfolio1_output.txt
+--SET ECHO ON
+--SPOOL sql_portfolio1_output.txt
 
 --****PLEASE ENTER YOUR DETAILS BELOW****
 --sql_portfolio1.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Tutorial Class No:
+--Student ID: 32713339
+--Student Name: Ooi Yu Zhang
+--Unit Code: FIT2094
+--Tutorial Class No: 3
 
+DROP TABLE policy PURGE;
 
 /*Task 1: CREATE table POLICY and non FK constraints*/
+CREATE TABLE policy( 
+    policy_id           NUMBER(7) NOT NULL,
+    prop_no             NUMBER(4) NOT NULL,
+    policy_startdate    Date      NOT NULL,
+    policy_type_code    CHAR(1)   NOT NULL,
+    policy_length       NUMBER(3) NOT NULL,
+    insurer_code        CHAR(3)   NOT NULL
+);
 
+COMMENT ON COLUMN policy.policy_id IS
+    'Policy ID (unique for each policy record)';
 
-
+COMMENT ON COLUMN policy.prop_no IS
+    'Property number';
+    
+ALTER TABLE policy
+    ADD CONSTRAINT policy_pk PRIMARY KEY (policy_id);
+    
+ALTER TABLE policy
+    ADD CONSTRAINT unique_policy_startdate UNIQUE (policy_startdate);
 
 /*Task 1: Add FK constraints*/
-
-
-
+ALTER TABLE policy
+    ADD CONSTRAINT prop_no_fk FOREIGN KEY (prop_no) 
+        REFERENCES property (prop_no);
+        
+ALTER TABLE policy
+    ADD CONSTRAINT unique_prop_no UNIQUE (prop_no);
+    
+ALTER TABLE policy
+    ADD CONSTRAINT policy_typecode_fk FOREIGN KEY (policy_type_code)
+        REFERENCES policy_type (policy_type_code);
+        
+ALTER TABLE policy
+    ADD CONSTRAINT unique_policy_typecode UNIQUE (policy_type_code);
+    
+ALTER TABLE policy
+    ADD CONSTRAINT insurer_code FOREIGN KEY (insurer_code)
+        REFERENCES insurer (insurer_code);
+        
 /*Task 2*/
 
 
@@ -33,5 +66,5 @@ SPOOL sql_portfolio1_output.txt
 
 
 --Comment out SET ECHO and SPOOL commands before submitting your portfolio
-SPOOL OFF
-SET ECHO OFF
+--SPOOL OFF
+--SET ECHO OFF
