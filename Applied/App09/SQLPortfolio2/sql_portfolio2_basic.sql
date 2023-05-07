33a388367543e866b82b0d90173ea5b824c6dfaa
Ooi Yu Zhang <yooi0010@student.monash.edu>
2023-05-07 16:46:21 +0800

    Added work for Task 2 in SQL Portfolio 2.

--- Applied/App09/SQLPortfolio2/sql_portfolio2_basic.sql
+++ Applied/App09/SQLPortfolio2/sql_portfolio2_basic.sql
@@ -16,19 +16,24 @@
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
 select maint_id, to_char(maint_datetime, 'dd-Mon-yyyy') as "MAINT_DATETIME", prop_address, owner_givname || ' ' || owner_famname as "OWNER_NAME", maint_desc, maint_cost
-from rent.owner natural join rent.property natural join rent.maintenance
-where maint_cost > 1000 and maint_cost < 3000 and upper(maint_assigned) = 'Y'
+from rent.owner 
+    natural join rent.property 
+    natural join rent.maintenance
+where maint_cost > 1000 
+    and maint_cost < 3000 
+    and upper(maint_assigned) = 'Y'
 order by maint_cost desc, maint_datetime;
-
-
 
 /*2*/
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
-
-
-
+select r.rent_agreement_no, r.tenant_no, t.tenant_title || '. ' || t.tenant_givname || ' ' || t.tenant_famname as "TENANT_NAME", p.prop_address, o.owner_givname || ' ' || o.owner_famname as "OWNER_NAME", r.rent_lease_period || ' months' as "RENT_LEASE_PERIOD"
+from rent.tenant t 
+    join rent.rent r on t.tenant_no = r.tenant_no
+    join rent.property p on p.prop_no = r.prop_no
+    join rent.owner o on o.owner_no = p.owner_no
+order by r.rent_lease_period desc, r.tenant_no;
 
 --Comment out SET ECHO and SPOOL commands before submitting your portfolio
 --SPOOL OFF
