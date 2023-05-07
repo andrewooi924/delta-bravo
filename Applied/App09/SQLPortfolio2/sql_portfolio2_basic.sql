a784374d0eeeddc6fcce208797f4aca0913d30f0
Ooi Yu Zhang <yooi0010@student.monash.edu>
2023-05-07 15:58:04 +0800

    Added template for SQL Portfolio 2.

---
+++ Applied/App09/SQLPortfolio2/sql_portfolio2_basic.sql
@@ -0,0 +1,31 @@
+--Comment out SET ECHO and SPOOL commands before submitting your portfolio
+SET ECHO ON
+SPOOL sql_portfolio2_basic_output.txt
+
+--****PLEASE ENTER YOUR DETAILS BELOW****
+--sql_portfolio2_basic.sql
+
+--Student ID:
+--Student Name:
+--Unit Code:
+--Tutorial Class No:
+
+
+/*1*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+
+/*2*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+
+
+--Comment out SET ECHO and SPOOL commands before submitting your portfolio
+SPOOL OFF
+SET ECHO OFF
