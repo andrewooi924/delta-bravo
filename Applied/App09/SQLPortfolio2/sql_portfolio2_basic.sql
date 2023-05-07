1709569c44bc1576883acd4d9bd01c6e4c15c332
Ooi Yu Zhang <yooi0010@student.monash.edu>
2023-05-07 16:29:08 +0800

    Added work for Task 1 in SQL Portfolio 2.

--- Applied/App09/SQLPortfolio2/sql_portfolio2_basic.sql
+++ Applied/App09/SQLPortfolio2/sql_portfolio2_basic.sql
@@ -1,20 +1,24 @@
 --Comment out SET ECHO and SPOOL commands before submitting your portfolio
-SET ECHO ON
-SPOOL sql_portfolio2_basic_output.txt
+--SET ECHO ON
+--SPOOL sql_portfolio2_basic_output.txt
 
 --****PLEASE ENTER YOUR DETAILS BELOW****
 --sql_portfolio2_basic.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Tutorial Class No:
+--Student ID: 32713339
+--Student Name: Ooi Yu Zhang
+--Unit Code: FIT2094
+--Tutorial Class No: 3
 
 
 /*1*/
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
+select maint_id, to_char(maint_datetime, 'dd-Mon-yyyy') as "MAINT_DATETIME", prop_address, owner_givname || ' ' || owner_famname as "OWNER_NAME", maint_desc, maint_cost
+from rent.owner natural join rent.property natural join rent.maintenance
+where maint_cost > 1000 and maint_cost < 3000 and upper(maint_assigned) = 'Y'
+order by maint_cost desc, maint_datetime;
 
 
 
@@ -27,5 +31,5 @@ SPOOL sql_portfolio2_basic_output.txt
 
 
 --Comment out SET ECHO and SPOOL commands before submitting your portfolio
-SPOOL OFF
-SET ECHO OFF
+--SPOOL OFF
+--SET ECHO OFF
