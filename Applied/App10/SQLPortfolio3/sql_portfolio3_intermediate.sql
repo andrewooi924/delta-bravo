907195222e99500ec8a3fbad348dd062854e5784
Ooi Yu Zhang <yooi0010@student.monash.edu>
2023-05-10 13:31:30 +0800

    Added work for Task 1 for SQL Portfolio 3.

--- Applied/App10/SQLPortfolio3/sql_portfolio3_intermediate.sql
+++ Applied/App10/SQLPortfolio3/sql_portfolio3_intermediate.sql
@@ -1,14 +1,14 @@
 --Comment out SET ECHO and SPOOL commands before submitting your portfolio
-SET ECHO ON
-SPOOL sql_portfolio3_intermediate_output.txt
+--SET ECHO ON
+--SPOOL sql_portfolio3_intermediate_output.txt
 
 --****PLEASE ENTER YOUR DETAILS BELOW****
 --sql_portfolio3_intermediate.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Tutorial Class No:
+--Student ID: 32713339
+--Student Name: Ooi Yu Zhang
+--Unit Code: FIT2094
+--Tutorial Class No: 03
 
 
 /*1*/
@@ -16,6 +16,11 @@ SPOOL sql_portfolio3_intermediate_output.txt
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
 
+select tenant_no, tenant_title || '. ' || tenant_givname || ' ' || tenant_famname as "Tenant Name", count(dam_no) as "No. of Damages", to_char(sum(dam_cost), '$9990.99') as "Total Cost"
+from rent.tenant natural join rent.rent natural join rent.damage
+where to_char(dam_datetime, 'yyyy') = '2022'
+group by tenant_no, tenant_title, tenant_givname, tenant_famname
+order by "Total Cost" desc, tenant_no;
 
 /*2*/
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
@@ -25,5 +30,5 @@ SPOOL sql_portfolio3_intermediate_output.txt
 
 
 --Comment out SET ECHO and SPOOL commands before submitting your portfolio
-SPOOL OFF
-SET ECHO OFF
+--SPOOL OFF
+--SET ECHO OFF
