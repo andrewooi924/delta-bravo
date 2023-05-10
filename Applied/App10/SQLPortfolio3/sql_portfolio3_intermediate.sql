2773a44c71faefba1e4f99c4fad39f09f00c238c
Ooi Yu Zhang <yooi0010@student.monash.edu>
2023-05-10 12:09:42 +0800

    Uploaded templates for Week 10 Tutorial and SQL Portfolio 3.

---
+++ Applied/App10/SQLPortfolio3/sql_portfolio3_intermediate.sql
@@ -0,0 +1,29 @@
+--Comment out SET ECHO and SPOOL commands before submitting your portfolio
+SET ECHO ON
+SPOOL sql_portfolio3_intermediate_output.txt
+
+--****PLEASE ENTER YOUR DETAILS BELOW****
+--sql_portfolio3_intermediate.sql
+
+--Student ID:
+--Student Name:
+--Unit Code:
+--Tutorial Class No:
+
+
+/*1*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+/*2*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+
+--Comment out SET ECHO and SPOOL commands before submitting your portfolio
+SPOOL OFF
+SET ECHO OFF
---
+++ Applied/App10/week10_sql_intermediate.sql
@@ -0,0 +1,72 @@
+/*
+Databases Week 10 Tutorial Class
+week10_sql_intermediate.sql
+
+student id: 
+student name:
+last modified date:
+
+*/
+
+--1
+
+
+
+--2
+
+
+
+--3
+
+
+
+--4
+-- a. 
+
+
+
+-- b. Repeat students are only counted once across 2019
+
+
+
+--5
+
+
+
+
+--6
+
+
+
+--7
+
+
+
+--8
+
+
+
+--9
+
+
+
+--10
+
+
+
+
+--11
+
+
+
+--12
+
+
+
+--13
+
+
+
+
+
+
