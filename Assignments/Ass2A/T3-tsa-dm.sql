ce1fcfa972ab75fcd9a60d165bdd73e0c742dc9d
Ooi Yu Zhang <yooi0010@student.monash.edu>
2023-05-11 22:48:44 +0800

    Add work for Task3a for Assignment 2A

--- Assignments/Ass2A/T3-tsa-dm.sql
+++ Assignments/Ass2A/T3-tsa-dm.sql
@@ -1,10 +1,10 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T3-tsa-dm.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 32713339
+--Student Name: Ooi Yu Zhang
+--Unit Code: FIT2094
+--Applied Class No: 3
 
 /* Comments for your marker:
 
@@ -20,9 +20,11 @@ SET SERVEROUTPUT ON
 ---**end**---
 
 --3(a)
-
+DROP SEQUENCE booking_seq;
+CREATE SEQUENCE booking_seq START WITH 100 INCREMENT BY 10;
 
 --3(b)
+
 
 
 --3(c)
