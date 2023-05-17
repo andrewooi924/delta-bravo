4f7f4e0a3bc6069fcbde604bff023602806020b4
Ooi Yu Zhang <yooi0010@student.monash.edu>
2023-05-17 11:58:58 +0800

    Added template for Week 11 Tutorial

---
+++ Applied/App11/week11_sql_advanced.sql
@@ -0,0 +1,60 @@
+/*
+Databases Week 11 Tutorial Class
+week11_sql_advanced.sql
+
+student id: 
+student name:
+last modified date:
+
+*/
+--1
+
+
+--2
+
+
+
+--3
+
+
+--4
+
+/* Using outer join */
+
+
+
+/* Using set operator MINUS */
+
+
+
+/* Using subquery */
+
+
+
+--5
+
+
+
+--6
+
+
+
+--7
+
+
+
+--8
+
+
+    
+--9
+
+
+    
+--10
+
+
+
+
+
+
