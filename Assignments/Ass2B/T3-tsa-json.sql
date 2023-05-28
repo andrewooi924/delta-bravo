04eb775f5a6951841f9c99eafb2c6db973576087
Ooi Yu Zhang <yooi0010@student.monash.edu>
2023-05-29 02:51:02 +0800

    Added work for Question a for Task 3 for Assignment 2B

--- Assignments/Ass2B/T3-tsa-json.sql
+++ Assignments/Ass2B/T3-tsa-json.sql
@@ -1,10 +1,10 @@
 --*****PLEASE ENTER YOUR DETAILS BELOW*****
 --T3-tsa-json.sql
 
---Student ID:
---Student Name:
---Unit Code:
---Applied Class No:
+--Student ID: 32713339
+--Student Name: Ooi Yu Zhang
+--Unit Code: FIT2094
+--Applied Class No: 3
 
 /* Comments for your marker:
 
@@ -19,3 +19,42 @@
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
 
+SELECT
+    JSON_OBJECT ( '_id' VALUE town_id, 
+                  'name' VALUE town_name || ' ' || town_state,
+                  'location' VALUE JSON_OBJECT (
+                                'latitude' VALUE town_lat,
+                                'longitude' VALUE town_long),
+                  'avg_temperature' VALUE JSON_OBJECT (
+                                'summer' VALUE town_avg_summer_temp,
+                                'winter' VALUE town_avg_winter_temp),
+                  'no_of_resorts' VALUE count(resort_id),
+                  'resorts' VALUE JSON_ARRAYAGG(JSON_OBJECT(
+                                '_id' VALUE resort_id,
+                                'name' VALUE resort_name,
+                                'address' VALUE resort_street_address,
+                                'phone' VALUE resort_phone,
+                                'year_built' VALUE resort_yr_built_purch,
+                                'company_name' VALUE company_name))
+    FORMAT JSON )
+    || ','
+FROM
+    tsa.town
+    NATURAL JOIN tsa.resort
+    NATURAL JOIN tsa.company
+GROUP BY
+    town_id,
+    town_name,
+    town_state,
+    town_lat,
+    town_long,
+    town_avg_summer_temp,
+    town_avg_winter_temp,
+    resort_id,
+    resort_name,
+    resort_street_address,
+    resort_phone,
+    resort_yr_built_purch,
+    company_name
+ORDER BY
+    town_id;
